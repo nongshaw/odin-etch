a41264be22776827b5c7abf00589e7d8a7ac42a4
nongshaw <141841360+nongshaw@users.noreply.github.com>
2023-11-09 13:18:08 +0000

    Add sketching functionality and all buttons

--- sketch.js
+++ sketch.js
@@ -1,4 +1,50 @@
-// Create 16x16 grid
-let gridBox = document.querySelector(".grid-box");
+console.log("hi");
 
-gridBox
+let color = "black";
+
+
+// Create 16x16 grid
+function createPad(size){
+    let pad = document.querySelector(".pad");
+    let squares = pad.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    pad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    pad.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let amount = size * size;
+    for(let i = 0; i < amount; i++) {
+        let square = document.createElement("div");
+        square.addEventListener("mouseover", colorSquare);
+        square.style.backgroundColor = "white";
+        pad.insertAdjacentElement("beforeend", square);
+    }
+}
+
+createPad(16);
+
+// Enable user to change size of sketchpad
+function changeSize(input) {
+    if(input >=2 && input <=100) {
+        createPad(input);
+    } else {
+        console.log("invalid input");
+    }
+}
+
+function colorSquare() {
+    if (color === 'random') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(selection) {
+    color = selection;
+}
+
+function resetPad() {
+    let pad = document.querySelector(".pad");
+    let squares = pad.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = "white"); 
+}
